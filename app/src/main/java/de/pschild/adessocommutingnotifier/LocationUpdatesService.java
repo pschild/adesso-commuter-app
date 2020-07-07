fff4e12084603ba5d2362d59fbf029f3b0297f5c
Schild, Philippe <Philippe.Schild@adesso.de>
2020-07-07 20:49:51 +0200

    update msg

--- app/src/main/java/de/pschild/adessocommutingnotifier/LocationUpdatesService.java
+++ app/src/main/java/de/pschild/adessocommutingnotifier/LocationUpdatesService.java
@@ -5,17 +5,23 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LocationUpdatesService extends Service {
 
   private static final String TAG = LocationUpdatesService.class.getSimpleName();
-  public static final String CHANNEL_ID = "ForegroundServiceChannel";
+  private static final String CHANNEL_ID = "ForegroundServiceChannel";
+  private static final int NOTIFICATION_ID = 12345678;
+
+  private int mCounter = 0;
 
   @Override
   public void onCreate() {
@@ -26,19 +32,9 @@ public class LocationUpdatesService extends Service {
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
     Log.d(TAG, "LocationUpdatesService.onStartCommand");
-    String input = intent.getStringExtra("inputExtra");
     createNotificationChannel();
-    Intent notificationIntent = new Intent(this, MainActivity.class);
-    PendingIntent pendingIntent = PendingIntent.getActivity(this,
-        0, notificationIntent, 0);
 
-    Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-        .setContentTitle("Foreground Service")
-        .setContentText(input)
-        .setSmallIcon(R.drawable.ic_launcher_background)
-        .setContentIntent(pendingIntent)
-        .build();
-    startForeground(1, notification);
+    startForeground(NOTIFICATION_ID, this.buildNotification(intent.getStringExtra("inputExtra")));
 
     doHeavyWorkOnABackgroundThread();
 
@@ -58,16 +54,47 @@ public class LocationUpdatesService extends Service {
     return null;
   }
 
+  private Notification buildNotification(String text) {
+    Intent notificationIntent = new Intent(this, MainActivity.class);
+    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+    return new NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle("Foreground Service")
+        .setContentText(text)
+//        .setOngoing(true)
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentIntent(pendingIntent)
+        .build();
+  }
+
+  private void updateNotification(String text) {
+    Notification notification = buildNotification(text);
+    NotificationManager mNotificationManager = (NotificationManager) getSystemService(
+        Context.NOTIFICATION_SERVICE);
+    mNotificationManager.notify(NOTIFICATION_ID, notification);
+  }
+
   private void doHeavyWorkOnABackgroundThread() {
     Log.d(TAG, "LocationUpdatesService.doHeavyWorkOnABackgroundThread");
-    new android.os.Handler().postDelayed(
-        new Runnable() {
-          public void run() {
-            Log.i(TAG, "This'll run 300 milliseconds later");
-            stopSelf();
-          }
-        },
-        5000);
+    // timeout
+//    new android.os.Handler().postDelayed(
+//        new Runnable() {
+//          public void run() {
+//            Log.i(TAG, "This'll run 300 milliseconds later");
+//            stopSelf();
+//          }
+//        },
+//        5000);
+
+    // interval
+//    new Timer().scheduleAtFixedRate(new TimerTask() {
+//      @Override
+//      public void run() {
+//        Log.i(TAG, "each 2s");
+//        updateNotification("x=" + mCounter);
+//        mCounter++;
+//      }
+//    }, 0, 2000);
   }
 
   private void createNotificationChannel() {
