6bdcec4acb6da86d2cecccf469dbe30ce9d9fba5
Schild, Philippe <Philippe.Schild@adesso.de>
2020-07-07 18:54:42 +0200

    initial commit

---
+++ app/src/main/java/de/pschild/adessocommutingnotifier/MainActivity.java
@@ -0,0 +1,18 @@
+package de.pschild.adessocommutingnotifier;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
